2b827beaac3161f5dc1e172a7882e0f82e9267e4
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-10 02:27:47 +0700

    сделал четыре заготовки фргаментов

---
+++ app/src/main/java/com/artyom/gpstracker_hdbh/fragments/SettigsFragment.kt
@@ -0,0 +1,46 @@
+package com.artyom.gpstracker_hdbh.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artyom.gpstracker_hdbh.databinding.FragmentMainBinding
+import com.artyom.gpstracker_hdbh.databinding.SettingsBinding
+import com.artyom.gpstracker_hdbh.databinding.ViewTrackBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ * Фрагмент отобраает окно настоек приложения
+ */
+class SettingsFragment : Fragment() {
+    private lateinit var binding: SettingsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        // Inflate the layout for this fragment
+        binding = SettingsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
