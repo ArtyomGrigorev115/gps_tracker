813494aef322cb5a4bea873e0b042bd26c9e78e4
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-10 01:40:33 +0700

    добавил нижнее меню

--- app/src/main/java/com/artyom/gpstracker_hdbh/MainActivity.kt
+++ app/src/main/java/com/artyom/gpstracker_hdbh/MainActivity.kt
@@ -2,7 +2,9 @@ package com.artyom.gpstracker_hdbh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.artyom.gpstracker_hdbh.databinding.ActivityMainBinding
+import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        onBottomNavClicks()
+    }
+
+    private fun onBottomNavClicks(){
+        binding.bNan.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                R.id.id_tracks -> Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+                R.id.id_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+            true
+        }
     }
 }
