7e650681f5f898c3754e271e43a797868d05ee36
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-19 01:10:46 +0700

    обновления местоположения

--- app/src/main/java/com/artyom/gpstracker_hdbh/location/LocationService.kt
+++ app/src/main/java/com/artyom/gpstracker_hdbh/location/LocationService.kt
@@ -1,18 +1,32 @@
 package com.artyom.gpstracker_hdbh.location
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import com.artyom.gpstracker_hdbh.MainActivity
 import com.artyom.gpstracker_hdbh.R
+import com.google.android.gms.location.*
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 
 class LocationService : Service() {
+
+    /*FusedLocationProviderClient*/
+    private lateinit var locProvider: FusedLocationProviderClient
+
+    /**/
+    private lateinit var locRequest: LocationRequest
+
+
     override fun onBind(intent: Intent?): IBinder? {
         //Коммуникационный канал с активностью
         return null
@@ -30,6 +44,7 @@ class LocationService : Service() {
         * id - 10 идентификатор сервиса
         * notification - уведомление пользоватея в статус баре о том, что в фоновом режиме что-то работает */
         startNotification()
+        startLocationUpdates()
         isRunning = true
         return START_STICKY
     }
@@ -37,6 +52,7 @@ class LocationService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d("MyLog", "Сервис запустился: onCreate()")
+        initLocation()
 
 
     }
@@ -45,7 +61,31 @@ class LocationService : Service() {
         super.onDestroy()
         Log.d("MyLog", "Сервис уничтожился: onDestroy()")
         isRunning = false
+        locProvider.removeLocationUpdates(lockCallBack)
     }
+
+    /*callback для locProvider.requestLocationUpdates
+    * принимает данные о местоположении от FusedLocationProviderClient*/
+    private val lockCallBack = object : LocationCallback(){
+
+        /*сведения о местоположении доступны*/
+        override fun onLocationAvailability(p0: LocationAvailability) {
+            super.onLocationAvailability(p0)
+        }
+
+        /*получить результат о местоположении*/
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+
+            /*последнее известное местоположение смартфна*/
+            //locationResult.lastLocation
+
+            Log.d("MyLog", "Местонахождение ${locationResult.lastLocation?.latitude}")
+        }
+    }
+
+
+
 
     /*Запуск  сервиса  в приоритетном фоновом режиме
      * пользователь увидит уведмление, что сервис запущен.
@@ -77,6 +117,51 @@ class LocationService : Service() {
         /*Запуск  сервиса  в приоритетном фоновом режиме показвает уведомление*/
         startForeground(99,notification)
 
+    }
+
+    /*инициализация объекта FusedLocationProviderClient*/
+    private fun initLocation(){
+        locRequest = LocationRequest.create()
+
+        /*интервал обновлений, который в настройках выберает пользователь*/
+        locRequest.interval = 5000
+
+        /*Самый быстрый интервал обновления местоположения*/
+        locRequest.fastestInterval = 5000
+
+        locRequest.priority = PRIORITY_HIGH_ACCURACY
+
+        locProvider = LocationServices.getFusedLocationProviderClient(baseContext)
+    }
+
+    /*Подписывается на получение сведений о местоположении*/
+    private fun startLocationUpdates(){
+
+        /*Проверка разрешений*/
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        /*запросить сведения о местоположении*/
+        locProvider.requestLocationUpdates(
+            locRequest,
+            lockCallBack,
+            Looper.myLooper()
+        )
     }
 
 
