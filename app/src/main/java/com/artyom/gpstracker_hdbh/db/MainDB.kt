5cdcd11570be46e4d8c835c6f1f8c392fba8fba8
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-22 23:17:09 +0700

    Создал БД

---
+++ app/src/main/java/com/artyom/gpstracker_hdbh/db/MainDB.kt
@@ -0,0 +1,40 @@
+package com.artyom.gpstracker_hdbh.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TrackItem::class], version = 1)
+ abstract class MainDb : RoomDatabase() {
+
+    /*DAO*/
+
+
+
+    companion object{
+        @Volatile
+         var INSTANCE: MainDb? = null
+
+       /* fun getDatabase(context: Context) : MainDb{
+            if(INSTANCE != null){
+                return INSTANCE as MainDb
+            }
+            else{
+                synchronized(this){
+                    val instance = Room.databaseBuilder(context.applicationContext, MainDb::class.java, "GpsTracker.db").build()
+                    return instance
+                }
+            }
+        }*/
+
+        fun getDatabase(context: Context) : MainDb{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext, MainDb::class.java, "GpsTracker.db").build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+}
